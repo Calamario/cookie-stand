d4417c260dfc3ea95ff2c034211ec899a32563b1
MARIO NISHIO <mario.nishio@gmail.com>
2018-04-09 13:02:14 -0700

    Made a function that renders the hard coded sales info

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function renderSales() {
+  // get location of parent
+  var ulEl = document.getElementById('pikeAndFirst');
+  // create an element
+  var liEl = document.createElement('li');
+  // give the element content
+  liEl.textContent = '6am: 16 cookies';
+  // append the element
+  ulEl.appendChild(liEl);
+}
+
+renderSales();

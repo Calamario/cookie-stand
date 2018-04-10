51350536c26d9715eae5e898f84d08e2d58401a0
MARIO NISHIO <mario.nishio@gmail.com>
2018-04-10 10:57:03 -0700

    Thought all stores close at 8pm instead of 9pm

--- js/app.js
+++ js/app.js
@@ -1,8 +1,54 @@
 'use strict';
 
+// function store(minCustomer, maxCustomer, avgCookieSale, parentLocation) {
+//   this.open = 6;
+//   this.close = 8;
+//   this.minCustomer = minCustomer;
+//   this.maxCustomer = maxCustomer;
+//   this.avgCookieSale = avgCookieSale;
+//   this.hourlySaleArray = [];
+//   this.totalSold = 0;
+//   // get location of parent
+//   this.ulEl = document.getElementById(parentLocation);
+//   this.cookieTime = function() {
+//     var hoursOfOperation = 12 - this.open + this.close;
+//     var totalCookiesSold = 0;
+//     for(var i = 0; i <= hoursOfOperation; i++) {
+//       // Generates random number of customer for that hour
+//       var randomCustomerNumber = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+//       // Calculates the amount of cookies sold that hour
+//       var cookiesSoldByHour = Math.round(this.avgCookieSale * randomCustomerNumber);
+//       // Add to total cookie count
+//       totalCookiesSold += cookiesSoldByHour;
+//       // push into the array to store data
+//       this.hourlySaleArray.push(cookiesSoldByHour);
+//       // create an element
+//       var liEl = document.createElement('li');
+//       // if afternoon put pm and change back from military time, if before noon put am and give the list item the content
+//       if (this.open + i > 12) {
+//         liEl.textContent = (this.open + i - 12) + 'pm: ' + cookiesSoldByHour + ' cookies';
+//       } else if (this.open + i < 12){
+//         // give the element content
+//         liEl.textContent = this.open + i + 'am: ' + cookiesSoldByHour + ' cookies';
+//       } else {
+//         liEl.textContent = '12pm: ' + cookiesSoldByHour + ' cookies';
+//       }
+//       // append the element
+//       this.ulEl.appendChild(liEl);
+//     }
+//     this.totalSold = totalCookiesSold;
+//     liEl.textContent = 'Total: ' + totalCookiesSold + ' cookies';
+//     this.ulEl.appendChild(liEl);
+//   };
+// }
+
+
+
+
+
 var pikeAndFirst = {
   open: 6,
-  close: 8,
+  close: 9,
   minCustomer: 23,
   maxCustomer: 65,
   avgCookieSale: 6.3,
@@ -43,7 +89,7 @@ var pikeAndFirst = {
 
 var seaTacAir = {
   open: 6,
-  close: 8,
+  close: 9,
   minCustomer: 3,
   maxCustomer: 24,
   avgCookieSale: 1.2,
@@ -77,7 +123,7 @@ var seaTacAir = {
 
 var seattleCenter = {
   open: 6,
-  close: 8,
+  close: 9,
   minCustomer: 11,
   maxCustomer: 38,
   avgCookieSale: 3.7,
@@ -110,7 +156,7 @@ var seattleCenter = {
 
 var capitolHill = {
   open: 6,
-  close: 8,
+  close: 9,
   minCustomer: 20,
   maxCustomer: 38,
   avgCookieSale: 2.3,
@@ -143,7 +189,7 @@ var capitolHill = {
 
 var alki = {
   open: 6,
-  close: 8,
+  close: 9,
   minCustomer: 2,
   maxCustomer: 16,
   avgCookieSale: 4.6,
